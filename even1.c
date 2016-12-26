385b81a87af8464751e9ed9eea28cbbefe704c63
mahashanmugam <mahashanmugam33@gmail.com>
2016-12-26 00:38:58 -0800

    Add files via upload

---
+++ even1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("\n enter the number");
+scanf("%d",&n);
+if(n%2==0)
+printf("\n %d is even number");
+else
+printf("\n %d is odd number");
+getch();
+return 0;
+}
