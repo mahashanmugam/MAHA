923d13bc39a969a209887569153626c52a63669e
mahashanmugam <mahashanmugam33@gmail.com>
2016-12-26 02:15:03 -0800

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.H>
+main()
+{
+int i=1,n,sum=0;;
+printf("\n enter the number:");
+scanf("%d",&n);
+while(i<=n                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             )
+{
+sum=sum+i;
+i++;
+}
+printf("sum=%d",sum);
+getch();
+}
