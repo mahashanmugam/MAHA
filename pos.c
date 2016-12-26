96c45c41d24182f921961c350edffaa445a73816
mahashanmugam <mahashanmugam33@gmail.com>
2016-12-25 22:35:21 -0800

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num;
+printf("enter the num");
+scanf("%d",&num);
+if(num>0)
+printf("\n positive");
+else
+printf("\n negative");
+getch();
+}
