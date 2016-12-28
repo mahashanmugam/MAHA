0aac13cc785d73b3eecb36cf0863036529e6cbd3
mahashanmugam <mahashanmugam33@gmail.com>
2016-12-27 20:43:59 -0800

    Add files via upload

---
+++ stringrev.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char st1[20];
+printf("enter string to reverse");
+scanf("%s",&st1);
+strrev(st1);
+printf("\n string after reversing:%s",st1);
+}
